415d6892f73aa4755477acf8aa6797bf90b16532
Gochad <malgorzata.drag@spoton.com>
2024-12-14 22:45:41 +0100

    add logo

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -5,6 +5,18 @@ const Home: React.FC = () => {
   return (
     <Container maxWidth="md">
       <Box mt={4} textAlign="center">
+        <Box
+          component="div"
+          sx={{
+            backgroundImage: `url('logo.png')`,
+            backgroundSize: 'contain',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            width: '150px',
+            height: '150px',
+            margin: '0 auto 20px',
+          }}
+        />
         <Typography variant="h3" component="h1" gutterBottom>
           Welcome to the Running Event App
         </Typography>
